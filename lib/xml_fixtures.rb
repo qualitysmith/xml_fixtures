087b125791edf44e204814f9bce8a041a7de3afa
Michael Goff <michael.goff@qualitysmith.com>
2009-10-22 08:57:23 -0700

    Got rid of a require that was throwing off including as gem in plugins which required a more specific require to take it's place.

--- lib/xml_fixtures.rb
+++ lib/xml_fixtures.rb
@@ -1,5 +1,5 @@
-require 'test_help'
 require 'rexml/document'
 require 'rexml_overrides'
 require 'xml_fixture_helper'
+require 'action_view/erb/util'
 ActiveSupport::TestCase.send :include, XmlFixtureHelper
